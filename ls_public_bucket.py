24cdac3cfbdf0752ed3baca4d75d0441777c60ab
David Porter <david.a.porter@gmail.com>
2018-08-07 14:04:20 +1000

    Add more debugging output

--- ls_public_bucket.py
+++ ls_public_bucket.py
@@ -194,6 +194,7 @@ def get_metadata_docs(bucket_name, prefix, start, stop):
             for row in range(int(start[1]), int(stop[1]), 1):
                 prefix_new = (str(prefix) + '/' + str(format(path, '03d')) + '/' + str(format(row, '03d')))
                 logging.info("Processing prefix: %s", prefix_new)
+                logging.info("* Bucket objects: %s", bucket.objects)
                 for obj in bucket.objects.filter(Prefix=prefix_new):
                     logging.info("Processing obj: %s", obj.key)
                     if obj.key.endswith('MTL.txt') and (not obj.key.endswith('RT_MTL.txt')):
